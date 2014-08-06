5744bb7714b2dd75723073a1b7590cf221d2507e
Richard Horne <richierich1204@yahoo.com>
2014-08-05 19:29:02 -0500

    Update index and users controller to display all users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def create
+    @user = User.new(params[:name])
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
   end
 
   def show
